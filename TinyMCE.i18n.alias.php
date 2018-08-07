eda683a6c7f33672454c2b5b006982ce10621314
Umherirrender <umherirrender_de.wp@web.de>
2018-08-07 21:25:48 +0200

    Add special page aliases
    Change-Id: I07c24fda9f5a805e638304acb017f577ffb1423f

---
+++ TinyMCE.i18n.alias.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *  Aliases for special pages.
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'TinyMCEUploadWindow' => array( 'TinyMCEUploadWindow' ),
+);
