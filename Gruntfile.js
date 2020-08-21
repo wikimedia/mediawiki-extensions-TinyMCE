f93a82993f46055529fd1a74628847b861d322fa
Umherirrender <umherirrender_de.wp@web.de>
2020-08-21 18:52:35 +0200

    build: Add grunt-banana-checker
    banana-checker will test for valid i18n files
    
    Change-Id: I93d15d2e768bb66219a4b3ea1f608df2e3490ebb

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,10 @@
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
 
+	var conf = grunt.file.readJSON( 'extension.json' );
 	grunt.initConfig( {
+		banana: conf.MessagesDirs,
 		jsonlint: {
 			all: [
 				'**/*.json',
@@ -11,6 +14,6 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint' ] );
+	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
